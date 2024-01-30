64603d1749a8dc35f086ee5b8f05d4cdc94c0c38
didiotghima <nurlanuuulubeksultan@gmail.com>
2024-01-30 15:03:06 -0500

    очистка файла dependencies

--- app/user/dependencies.py
+++ app/user/dependencies.py
@@ -1,7 +1,8 @@
-from fastapi import Depends, Request, HTTPException, status
+from fastapi import Depends, Request 
 from jose import jwt, JWTError
 from app.config import settings
 from datetime import datetime
+from app.exeptions import TokenExpiredExcexption, TokenAbsentException, IncorrectTokenFormatException, UserIsNotPresentException
 
 from app.user.dao import UsersDAO
 from app.user.models import Users
@@ -10,7 +11,7 @@ from app.user.models import Users
 def get_token(request: Request):
     token = request.cookies.get("bookings_access_token")
     if not token :
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenAbsentException
     return token
 
 
@@ -23,15 +24,15 @@ async def get_current_user(token: str = Depends(get_token)):
         print(f"Decoded Payload: {payload}")
     except JWTError as e:
         print(f"Error decoding token: {e}")
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise IncorrectTokenFormatException
     expire: str = payload.get("exp")
     if  (not expire) or (int(expire) < datetime.utcnow().timestamp()):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise TokenExpiredExcexption
     user_id: str = payload.get("sub")
     if not user_id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotPresentException
     user = await UsersDAO.find_by_id(int(user_id))
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise UserIsNotPresentException
     
     return user
--- app/user/router.py
+++ app/user/router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException, Response, status
+from fastapi import APIRouter, Depends, Response
+from app.exeptions import  IncorrectEMailOrPasswordException, UserAlreadyExistsException
 from app.user.dao import UsersDAO
 from app.user.dependencies import  get_current_user
 from app.user.models import Users
@@ -18,7 +19,7 @@ async def register_user(user_data: SUserAuth):
     existing_user = await UsersDAO.find_one_or_none(email=user_data.email)
     if existing_user:
         # Если пользователь уже существует, вернуть ошибку
-        raise HTTPException(status_code=500)
+        raise UserAlreadyExistsException
     # Хеширование пароля
     hashed_password = get_password_hash(user_data.password)
     # Добавление нового пользователя
@@ -29,7 +30,7 @@ async def register_user(user_data: SUserAuth):
 async def login_user(response: Response, user_data: SUserAuth):
     user = await authenticate_user(user_data.email, user_data.password)
     if not user:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+        raise IncorrectEMailOrPasswordException
     access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return access_token
