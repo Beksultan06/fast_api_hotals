a8ae84562f9392b5bbf42be5eb335fc334de1159
didiotghima <nurlanuuulubeksultan@gmail.com>
2024-01-30 16:48:24 -0500

    запрос на базу данных

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,3 +1,4 @@
+from datetime import date
 from fastapi import APIRouter, Depends, Request
 from app.bookings.schemas import SBookings
 from app.bookings.dao import BookingDAO
@@ -14,7 +15,7 @@ async def get_bookings(user: Users = Depends(get_current_user)) -> list[SBooking
     return await BookingDAO.find_all(user_id=user.id)
 
 @router.post("")
-async def add_bookings(
+async def add_bookings(room_id: int, date_from: date, date_to: date,
     user: Users = Depends(get_current_user),
 ):
-    await BookingDAO.add()
+    await BookingDAO.add(user.id, room_id,date_from, date_to)
